cac419cd864e309a73416dc031f3cc7d218c83de
Alberto Camillo <alberto.camillo@terem.com.au>
2020-04-01 21:07:20 +1100

    First commit

---
+++ src/app/media-item-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.scss']
+})
+export class MediaItemFormComponent {
+  onSubmit(value) {
+    console.log(value);
+  }
+}
